86ffceb57007ea1f47930ba920e0a29478c81159
Ashokgit25ai <anithaashok944@gmail.com>
2026-06-26 17:04:48 +0530

    Handle error

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,45 @@ import Loader from "./Loading/Loader";
 
 const App = () => {
   const [products, setProducts] = useState([]);
-  const [loading,setLoading] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [visibleCount, setVisibleCount] = useState(10);
   const [error, setError] = useState("");
 
+  const fetchProducts = async () => {
+    setLoading(true);
+    setError("");
+    setProducts([]);
+
+    try {
+      const res = await axios.get("https://fakestoreapi.com/products");
+      setProducts(res.data);
+    } catch (err) {
+      setError("Failed to load products. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const res = await axios.get("https://fakestoreapi.com/products");
-        setProducts(res.data);
-      } catch (err) {
-        setError("Failed to load page");
-      }
-    };
     fetchProducts();
   }, []);
 
-  const visibleProducts = products.slice(0,visibleCount)
+  const visibleProducts = products.slice(0, visibleCount);
 
-  const showMore = () =>{
-    setVisibleCount((prev)=>prev+10);
-  }
+  const showMore = () => {
+    setVisibleCount((prev) => prev + 10);
+  };
 
   return (
     <div className="products-container">
-      <h2 className="title">Products List </h2>
-      <div>{error && <h1>{error}</h1>}</div>
+      <h2 className="title">API STORE</h2>
+      <div>{error && <div>
+         <h2 className="error">{error}</h2>
+         <button onClick={fetchProducts} className="retry-btn">Retry</button>
+        </div>
+        
+      }  
+      </div>
       <div>
         <div className="products-grid">
           {visibleProducts.map((product) => (
@@ -44,15 +57,12 @@ const App = () => {
             </div>
           ))}
         </div>
-        {
-          visibleCount < products.length && (
-            <button onClick={showMore} className="show-more-btn">
-              Show More
-            </button>
-          )
-        }
+        {visibleCount < products.length && (
+          <button onClick={showMore} className="show-more-btn">
+            Show More
+          </button>
+        )}
         <div>{loading && <Loader />}</div>
-        
       </div>
     </div>
   );
